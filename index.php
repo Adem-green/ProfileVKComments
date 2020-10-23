6a7a79b1be861b8d5fa20c3f08a87b280c3c3fd2
Vadim Valeev <vvaleev95@mail.ru>
2020-10-23 20:34:12 +0500

    Сделал возможность записи всех ошибок в лог. Иногда из-за верстки на сайте не удается увидеть выводимые ошибки PHP и поэтому лучше их логировать в отдельный лог-файл, чтоб можно было при возможности их там посмотреть

--- index.php
+++ index.php
@@ -4,6 +4,8 @@
 
     ini_set('display_errors', 1);
     ini_set('display_startup_errors', 1);
+    ini_set('log_errors', 1);
+    ini_set('error_log', __DIR__ . '/logs/error.log');
 
 $data = [
     'user' => [
