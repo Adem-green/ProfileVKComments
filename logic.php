3900d0ff13b01160a158263433cc49970f7a9397
adem <https://github.com/Adem-green/ProfileVKComments.git>
2020-12-27 19:03:43 +0500

    Добавил функций getPage и getUser.

--- logic.php
+++ logic.php
@@ -78,74 +78,89 @@ $data = [
     ],
 ];
 */
-//print_array($data);
-
-$data ['user'] = [
-    'first_name' => 'JOHN ',
-    'last_name' => 'DOE',
-    'position' => 'Software Engineer & UI/UX Expert email',
-    'about' => 'Рыбным текстом называется текст, служащий для временного наполнения макета в публикациях или производстве веб-сайтов, пока финальный текст еще не создан. Рыбный текст также известен как текст-заполнитель или же текст-наполнитель. Иногда текст-«рыба» также используется композиторами при написании музыки. Они напевают его перед тем, как сочинены соответствующие слова. Уже в 16-том веке рыбные тексты имели широкое распространение у печатников.',
-    'avatar' => '/images/avatar.jpg.',
-];
-$data ['page'] = [
-    'title' => 'ВК',
-    'cover' => '/images/fon2.jpg.',
-    'comment_block_title' => 'Комментарии',
-    'comment_text_placeholder' => 'Ваш комментарии...',
-    'comment_btn_caption' => 'Отправить',
-    'auth_subtitle' => 'Для продолжения вам необходимо войти',
-    'auth_subtitle_2' => 'ВКонтакте',
-    'auth_login_caption' => 'Телефон или email',
-    'auth_password_caption' => 'Пароль',
-    'auth_btn_caption' => 'Войти',
-];
-$data['comments']['0'] = [
-    'first_name' => 'Тлеген ',
-    'last_name' => 'Кустобаев',
-    'avatar' => 'images/comment-1.jpg',
-    'comment' => 'Прикольно',
-    'string_date' => 'Три часа назад',
-    'is_hidden' => false,
-    'background_color' => '#9decc3',
-];
-
-$data['comments']['1'] = [
-    'first_name' => 'Владислав ',
-    'last_name' => 'Смирнов',
-    'avatar' => 'images/comment-2.jpg',
-    'comment' => 'Это типо резюме?',
-    'string_date' => 'Вчера',
-    'is_hidden' => false,
-    'background_color' => '#ffff5e',
-];
-
-$data ['comments']['2'] = [
-    'first_name' => 'Анжелика ',
-    'last_name' => 'Зайцева',
-    'avatar' => 'images/comment-3.jpg',
-    'comment' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad architecto asperiores, commodi, cum debitis enim expedita inventore minima minus molestiae perferendis provident sequi tenetur? Blanditiis corporis eos iure neque saepe!',
-    'string_date' => '5 минут назад',
-    'is_hidden' => false,
-    'background_color' => '#f09bee',
-];
-
-$data ['comments']['3'] = [
-    'first_name' => 'Иван ',
-    'last_name' => 'Черных',
-    'avatar' => 'images/comment-4.jpg',
-    'comment' => 'Не думал что это сработает',
-    'string_date' => '2 секунды назад',
-    'is_hidden' => false,
-];
-
-$data ['comments']['4'] = [
-    'first_name' => 'Вадим ',
-    'last_name' => 'Валеев',
-    'avatar' => 'images/comment-5.jpg',
-    'comment' => 'Неправильно!!!',
-    'string_date' => '5 минут назад',
-    'is_hidden' => false,
-];
 
 
+$data['comments'] = getComments();
+$data['user'] = getUser();
+$data['page'] = getPage();
+
+function getUser() {
+    return [
+            'first_name' => 'JOHN ',
+            'last_name' => 'DOE',
+            'position' => 'Software Engineer & UI/UX Expert email',
+            'about' => 'Рыбным текстом называется текст, служащий для временного наполнения макета в публикациях или производстве веб-сайтов, пока финальный текст еще не создан. Рыбный текст также известен как текст-заполнитель или же текст-наполнитель. Иногда текст-«рыба» также используется композиторами при написании музыки. Они напевают его перед тем, как сочинены соответствующие слова. Уже в 16-том веке рыбные тексты имели широкое распространение у печатников.',
+            'avatar' => '/images/avatar.jpg.',
+    ];
+}
+
+function getPage() {
+    return [
+            'title' => 'ВК',
+            'cover' => '/images/fon2.jpg.',
+            'comment_block_title' => 'Комментарии',
+            'comment_text_placeholder' => 'Ваш комментарии...',
+            'comment_btn_caption' => 'Отправить',
+            'auth_subtitle' => 'Для продолжения вам необходимо войти',
+            'auth_subtitle_2' => 'ВКонтакте',
+            'auth_login_caption' => 'Телефон или email',
+            'auth_password_caption' => 'Пароль',
+            'auth_btn_caption' => 'Войти',
+    ];
+}
+
+function getComments() {
+    return [
+        [
+            'first_name' => 'Тлеген ',
+            'last_name' => 'Кустобаев',
+            'avatar' => 'images/comment-1.jpg',
+            'comment' => 'Прикольно',
+            'string_date' => 'Три часа назад',
+            'is_hidden' => false,
+            'background_color' => '#9decc3',
+        ],
+
+        [
+            'first_name' => 'Владислав ',
+            'last_name' => 'Смирнов',
+            'avatar' => 'images/comment-2.jpg',
+            'comment' => 'Это типо резюме?',
+            'string_date' => 'Вчера',
+            'is_hidden' => false,
+            'background_color' => '#ffff5e',
+        ],
+
+        [
+            'first_name' => 'Анжелика ',
+            'last_name' => 'Зайцева',
+            'avatar' => 'images/comment-3.jpg',
+            'comment' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad architecto asperiores, commodi, cum debitis enim expedita inventore minima minus molestiae perferendis provident sequi tenetur? Blanditiis corporis eos iure neque saepe!',
+            'string_date' => '5 минут назад',
+            'is_hidden' => false,
+            'background_color' => '#f09bee',
+        ],
+
+        [
+            'first_name' => 'Иван ',
+            'last_name' => 'Черных',
+            'avatar' => 'images/comment-4.jpg',
+            'comment' => 'Не думал что это сработает',
+            'string_date' => '2 секунды назад',
+            'is_hidden' => false,
+        ],
+
+        [
+            'first_name' => 'Вадим ',
+            'last_name' => 'Валеев',
+            'avatar' => 'images/comment-5.jpg',
+            'comment' => 'Неправильно!!!',
+            'string_date' => '5 минут назад',
+            'is_hidden' => false,
+        ],
+
+        [
+        ],
+    ];
+}
 
